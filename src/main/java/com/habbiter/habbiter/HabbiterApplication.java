3792c9a74ccdd8cb74a2de31fb795f84198a090a
Lewis Reid <lewis.reid148@Gmail.com>
2020-07-26 17:48:21 +0100

    Spring Boot Install

---
+++ src/main/java/com/habbiter/habbiter/HabbiterApplication.java
@@ -0,0 +1,22 @@
+package com.habbiter.habbiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HabbiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabbiterApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
+	}
+	
+}
---
+++ src/test/java/com/habbiter/habbiter/HabbiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habbiter.habbiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabbiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
