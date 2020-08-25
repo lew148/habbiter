9643b86d3ed94ba0cd959b890400f10731667a59
Lewis Reid <lewis.reid148@Gmail.com>
2020-08-25 22:50:32 +0100

    Added linting

---
+++ habbiter/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+    },
+    "plugins": [
+        "react",
+    ],
+    "settings": {
+        "react": {
+            "version": "detect",
+        },
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "react/prop-types": 0
+    },
+    "ignorePatterns": [
+        ".eslintrc.js",
+        "serviceWorker.js",
+    ],
+};
